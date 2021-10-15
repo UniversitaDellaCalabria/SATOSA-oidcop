3fa2747e82f1d7f24d037613d3788da3e8e5f3b7
peppelinux <giuseppe.demarco@unical.it>
2021-10-15 11:48:55 +0200

    initial

---
+++ README.md
@@ -0,0 +1,33 @@
+SATOSA oidcop frontend
+----------------------
+
+SATOSA Frontend based on idetity python oidcop.
+
+## Requirements
+
+- satosa
+- mongodb, see [Satosa-Saml2Spid mongo example](https://github.com/italia/Satosa-Saml2Spid/tree/oidcop/mongo).
+
+
+## Setup
+
+````
+pip install satosa_oidcop
+````
+
+## Demo
+
+[Satosa-Saml2Spid](https://github.com/italia/Satosa-Saml2Spid/) is a custom Satosa configuration to deal with many SAML2 and OIDC Relying parties and many eduGain and SPID Identity Provider.
+
+
+## Contributions
+
+Feel free to open issues and pull requests, we build communities!
+
+## Author
+
+Giuseppe De Marco <at> Università della Calabria
+
+## Credits
+
+Andrea Ranaldi <at> ISTRA Ambiente
---
+++ setup.py
@@ -0,0 +1,42 @@
+import re
+
+from glob import glob
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+_pkg_name = 'satosa_oidcop'
+
+with open(f'{_pkg_name}/__init__.py', 'r') as fd:
+    VERSION = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+setup(
+    name=_pkg_name,
+    version=VERSION,
+    description="SATOSA Frontend based on idetity python oidcop",
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'License :: OSI Approved :: GNU Affero General Public License v3',
+                 'Programming Language :: Python :: 3'],
+    url='https://github.com/UniversitaDellaCalabria/satosa-oidcop',
+    author='Giuseppe De Marco',
+    author_email='giuseppe.demarco@unical.it',
+    license='License :: OSI Approved :: GNU Affero General Public License v3',
+    scripts=[f'{_pkg_name}/bin/{_pkg_name}'],
+    packages=[f"{_pkg_name}"],
+    package_dir={f"{_pkg_name}": f"{_pkg_name}"},
+    package_data={f"{_pkg_name}": [
+            i.replace(f'{_pkg_name}/', '')
+            for i in glob(f'{_pkg_name}/**', recursive=True)
+        ]
+    },
+    install_requires=[
+        "satosa>=7.0.3",
+        "pymongo >= 3.11",
+        "oidcop == 2.2.0"
+    ],
+)
