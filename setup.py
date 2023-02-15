13f22fb713e41850a89872d297757139418a6c19
Andrea Ranaldi <andrea.ranaldi@gmail.com>
2023-02-15 09:42:25 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     },
     install_requires=[
         "satosa>=8.0.0",
-        "pymongo>=3.11,<=4.0.1",
-        "oidcop>=2.3.3,<=2.3.4"
+        "pymongo>=3.11,<=4.3",
+        "oidcop>=2.3.3,<=2.4"
     ],
 )
