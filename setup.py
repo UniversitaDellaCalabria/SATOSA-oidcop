4752c4b5de4866020c10e7acfc144b8de805136d
Pavel Břoušek <brousek@ics.muni.cz>
2023-06-21 11:01:47 +0200

    ci: refactoring of workflows, minor fixes
    * added commitlint to check for conventional commit messages
    * fixed Python version badge
    * added Pypi classifiers for each Python version
    * run CI checks on each push and pull request

--- setup.py
+++ setup.py
@@ -1,36 +1,43 @@
-import re
-
 from glob import glob
 from setuptools import setup
 
+
 def readme():
-    with open('README.md') as f:
+    with open("README.md") as f:
         return f.read()
 
-_pkg_name = 'satosa_oidcop'
+
+_pkg_name = "satosa_oidcop"
 
 setup(
     name=_pkg_name,
     description="SATOSA Frontend based on idetity python oidcop",
     long_description=readme(),
-    long_description_content_type='text/markdown',
-    classifiers=['Development Status :: 5 - Production/Stable',
-                 'License :: OSI Approved :: GNU Affero General Public License v3',
-                 'Programming Language :: Python :: 3'],
-    url='https://github.com/UniversitaDellaCalabria/satosa-oidcop',
-    author='Giuseppe De Marco',
-    author_email='giuseppe.demarco@unical.it',
-    license='License :: OSI Approved :: GNU Affero General Public License v3',
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    url="https://github.com/UniversitaDellaCalabria/satosa-oidcop",
+    author="Giuseppe De Marco",
+    author_email="giuseppe.demarco@unical.it",
+    license="License :: OSI Approved :: GNU Affero General Public License v3",
     packages=[f"{_pkg_name}"],
     package_dir={f"{_pkg_name}": f"{_pkg_name}"},
-    package_data={f"{_pkg_name}": [
-            i.replace(f'{_pkg_name}/', '')
-            for i in glob(f'{_pkg_name}/**', recursive=True)
+    package_data={
+        f"{_pkg_name}": [
+            i.replace(f"{_pkg_name}/", "")
+            for i in glob(f"{_pkg_name}/**", recursive=True)
         ]
     },
     install_requires=[
         "satosa>=8.0.0",
         "pymongo>=3.11,<=4.3",
-        "idpyoidc>=2.0.0,<=2.1.0"
+        "idpyoidc>=2.0.0,<=2.1.0",
     ],
 )
